85cc4ef43d012e8f53550b0939e89d538cb534df
jeffersonbarney <jefferson.barney@icloud.com>
2020-02-14 14:09:18 -0700

    fibonacci

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -39,7 +39,21 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+        int f[] = new int[n+2]; // 1 extra to handle case, n = 0
+        int i;
+
+        /* 0th and 1st number of the series are 0 and 1*/
+        f[0] = 0;
+        f[1] = 1;
+
+        for (i = 2; i <= n; i++)
+        {
+       /* Add the previous 2 numbers in the series
+         and store it */
+            f[i] = f[i-1] + f[i-2];
+        }
+
+        return f[n];
     }
 
 
